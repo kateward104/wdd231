53a1be5d5e2efd302bb0618244f8c7c60c0503ee
Katelyn Wardell <kateward104@gmail.com>
2025-10-04 09:48:49 -0600

    added to fetch.js

--- week3/js/fetch.js
+++ week3/js/fetch.js
@@ -1,16 +1,19 @@
 // fetch.js
 const url = "https://pokeapi.co/api/v2/pokemon/ditto";
+const urlList = "https://pokeapi.co/api/v2/pokemon";
 let results = null;
-async function getPokemon(url) {
+
+async function getPokemon(url, doThis) {
     const response = await fetch(url);
     //check to see if the fetch was successful
     if (response.ok) {
         // the API will send us JSON...but we have to convert the response before we can use it
         // .json() also returns a promise...so we await it as well.
         const data = await response.json();
-        doStuff(data);
+        doThis(data);
     }
 }
+
 function doStuff(data) {
     const text = document.querySelector("#output");
     results = data;
@@ -19,5 +22,23 @@ function doStuff(data) {
     text.innerHTML = html;
     console.log("first: ", results);
 }
-getPokemon(url);
+
+function doStuffList(data) {
+    console.log(data);
+    const pokeListElement = document.querySelector("#outputList");
+    const pokeList = data.results;
+    pokeList.forEach((currentItem) => {
+        const html = `<li>${currentItem.name}</li>`;
+        // note the += here...
+        pokeListElement.innerHTML += html;
+    });
+}
+
+function sortPokemon(list) {
+
+}
+
+getPokemon(url, doStuff);
 console.log("second: ", results);
+
+getPokemon(urlList, doStuffList);
